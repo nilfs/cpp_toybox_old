4ae4a13d582dd9ac43cf1e874b07d8624df11d0f
Nilfs <nilfs55_dev@gmail.com>
2013-06-29 15:13:28 +0900

    Initial Commit

---
+++ httpclient/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  httpclient
+//
+//  Created by nilfs on 13/06/29.
+//  Copyright (c) 2013年 nilfs. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
