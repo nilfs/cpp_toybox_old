cd9b6518cf0ca25e49d075f683088a03ce9dfa16
Nilfs <nilfs55_dev@gmail.com>
2014-09-01 23:52:43 +0900

    クラスを別ソースに分離

---
+++ RequestAndUpdate/RequestUpdate.cpp
@@ -0,0 +1,9 @@
+//
+//  RequestUpdate.cpp
+//  RequestAndUpdate
+//
+//  Created by nilfs on 2014/09/01.
+//  Copyright (c) 2014年 nilfs. All rights reserved.
+//
+
+#include "RequestUpdate.h"
---
+++ RequestAndUpdate/RequestUpdate.h
@@ -0,0 +1,14 @@
+//
+//  RequestUpdate.h
+//  RequestAndUpdate
+//
+//  Created by nilfs on 2014/09/01.
+//  Copyright (c) 2014年 nilfs. All rights reserved.
+//
+
+#ifndef __RequestAndUpdate__RequestUpdate__
+#define __RequestAndUpdate__RequestUpdate__
+
+#include <iostream>
+
+#endif /* defined(__RequestAndUpdate__RequestUpdate__) */
